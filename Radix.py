3aca7b1168fa5ea587d0cbbdc20d3edf2e7ae57a
Anecdotal <hduffy@students.stab.org>
2018-06-13 10:18:56 -0400

    unified file names

--- Radix.py
+++ Radix.py
@@ -15,8 +15,8 @@ def radix_sort(intList, exponent, radix):
 
 for i in range(100000):
 	radix_sort([1, 420, 1, 123, 15, 120, 987, 789, 69, 00], 3, 10)
-'''
 
+'''
 print radix_sort([19, 35, 20, 61, 14], 2, 10)
 print radix_sort([00, 420, 1, 123, 15], 3, 10)
 print radix_sort([35, 20, 69, 0], 2, 10)
@@ -25,5 +25,4 @@ print radix_sort([1, 2, 5, 9, 0, 0], 1, 10)
 print radix_sort([0b10, 0b01, 0b11, 0b100, 0b101], 3, 2) #Interesting because it shifts the binary to base 10 after sorting
 print radix_sort([0b10, 0b1100101, 0b11, 0b111, 0b1001], 6, 2)
 print radix_sort([0123, 0100, 07, 017, 0777], 3, 8)
-
 '''
