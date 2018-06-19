fec11313d4626174cc841e8f11b302e3e0fbd007
Anecdotal <hduffy@students.stab.org>
2018-06-19 08:38:36 -0400

    started gravity work

---
+++ gravity.py
@@ -0,0 +1,9 @@
+def gravity_sort(array):
+	#Construct bead structure
+	#Go Through Each Row
+	for i in range(len(rows)):
+		#Let 'Gravity' Take Its Course
+		
+	return array
+
+print gravity_sort([12000, 987, 789, 69, 14, 1])
