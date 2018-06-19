77e34a2b64cc87001894070d3ec32c686ecccf68
Anecdotal <hduffy@students.stab.org>
2018-06-19 08:37:05 -0400

    Finished Speed Tests

--- speed.py
+++ speed.py
@@ -1,9 +1,14 @@
+import random
 import time
 import radix
 import bogo
 import merge
 import heap
-import random
+import bubble
+import insertion
+import quick
+import selection
+#import gravity
 
 def speed_test(algo, generate_input, input_length, x):
 	start = time.clock()
@@ -30,13 +35,30 @@ def asymptotic_test(algo, generate_input, x):
 	for i in range(20):
 		array.append(speed_test(algo, generate_input, i, x))
 	return array
-
-print "Radix is"
-print asymptotic_test(radix.radix_sort2, generate_array, 10000)
-print "Merge is"
+'''
+print "Radix Sort:"
+print asymptotic_test(radix.radix_sort_base10, generate_array, 10000)
+print "Bubble Sort:"
+print asymptotic_test(bubble.bubble_sort, generate_array, 10000)
+print "Selection Sort:"
+print asymptotic_test(selection.selection_sort, generate_array, 10000)
+print "Insertion Sort:"
+print asymptotic_test(insertion.insertion_sort, generate_array, 10000)
+#print "Gravity Sort:"
+#print asymptotic_test(selection.selection_sort, generate_array, 10000)
+print "Quick Sort:"
+print asymptotic_test(quick.quick_sort, generate_array, 10000)
+print "Merge Sort:"
 print asymptotic_test(merge.merge_sort, generate_array, 10000)
-print "Heap is"
+print "Heap Sort:"
 print asymptotic_test(heap.heap_sort, generate_array, 10000)
+'''
+print speed_test(heap.heap_sort, generate_array, 100, 10000)
+print speed_test(heap.heap_sort, generate_array, 500, 10000)
+print speed_test(merge.merge_sort, generate_array, 100, 10000)
+print speed_test(merge.merge_sort, generate_array, 500, 10000)
+print speed_test(quick.quick_sort, generate_array, 500, 10000)
+print speed_test(radix.radix_sort_base10, generate_array, 500, 10000)
 
 
 
