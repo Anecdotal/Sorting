d9bbd2e489bb74f93d7bab6195642c9878829b3a
Anecdotal <hduffy@students.stab.org>
2018-06-19 23:09:21 -0400

    Wrote gravity sort

--- gravity.py
+++ gravity.py
@@ -1,9 +1,43 @@
 def gravity_sort(array):
 	#Construct bead structure
-	#Go Through Each Row
-	for i in range(len(rows)):
+	beads_not_built = 1
+	cols = []
+	while beads_not_built != 0:
+		list_sum = 0
+		col = []
+		for i in range(len(array)):
+			if array[i] != 0:
+				col.append(1) 
+				array[i] -= 1
+			else:
+				col.append(0)
+			list_sum += array[i]
+		cols.append(col)
+		beads_not_built = list_sum
+	#Go Through Each Column
+	for col in cols:
 		#Let 'Gravity' Take Its Course
-		
+		num_ones = 0
+		num_correct = 0
+		for bead in col:
+			num_ones += bead
+		i = len(col) - 1
+		while num_ones != 0:
+			if num_correct == num_ones:
+				if col[i] == 1:
+					col[i] = 0
+					num_ones -= 1
+					num_correct -= 1
+			else:
+				if col[i] == 0:
+					col[i] = 1
+					num_correct += 1
+				else:
+					num_ones -= 1
+			i -= 1
+	for element in array:
+		element = 0
+	for col in cols:
+		for i in range(len(col)):
+			array[i] += col[i]
 	return array
-
-print gravity_sort([12000, 987, 789, 69, 14, 1])
